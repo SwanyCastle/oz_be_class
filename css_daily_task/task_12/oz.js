590c9394769821ff729b2201db9969545f0cc8aa
SwanyCastle <seunghwan712@gmail.com>
2023-12-21 17:28:11 +0900

    javascript task commit

--- css_daily_task/task_12/oz.js
+++ css_daily_task/task_12/oz.js
@@ -1,20 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var clickableImage = document.getElementById('menu_image');
-    var targetElement = document.getElementById('target_element');
+document.addEventListener("DOMContentLoaded", function () {
+  var clickableImage = document.getElementById("menu_image");
+  var targetElement = document.getElementById("target_element");
 
-    var isClicked = false;
+  var isClicked = false;
 
-    clickableImage.addEventListener('click', function () {
-        isClicked = !isClicked;
+  clickableImage.addEventListener("click", function () {
+    isClicked = !isClicked;
 
-        if (isClicked) {
-            // Change CSS property of the target element
-            targetElement.style.display = "block";
-        } else {
-            targetElement.style.display = "none";
-        }
-    });
+    if (isClicked) {
+      // Change CSS property of the target element
+      targetElement.style.display = "block";
+    } else {
+      targetElement.style.display = "none";
+    }
+  });
 });
-
-
-
